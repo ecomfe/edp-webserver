15cfa56bbb70d7062377de4e0a7561461186bac7
leeight <leeight@gmail.com>
2014-03-12 11:04:22 +0800

    临时ci

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
+/**
+ * @file 开发时web调试服务器启动功能
+ * @author otakustay[otakustay@live.com], 
+ *         errorrik[errorrik@gmail.com],
+ *         ostream[ostream.song@gmail.com],
+ *         firede[firede@firede.us]
+ */
 
 exports.start = require( './lib/start' );
+
 exports.getDefaultConfig = function () {
     return require( './lib/config' );
 };
+
+/**
+ * @ignore
+ */
+exports.cli = require( './cli' );
